67d22f27441b7905c0ed0f51afc541dbcc150ac9
jhulbert-va <43861345+jhulbert-va@users.noreply.github.com>
2019-04-12 09:03:06 -0400

    Task/api 1040 namespace solution (#4)
    * Removed replaceAll from Queen Elizabeth
    
    * Updated sample for tests without namspace
    
    * Updated WSDL

--- queen-elizabeth/src/main/java/gov/va/api/health/queenelizabeth/util/XmlDocuments.java
+++ queen-elizabeth/src/main/java/gov/va/api/health/queenelizabeth/util/XmlDocuments.java
@@ -60,7 +60,7 @@ public final class XmlDocuments {
       transformerFactory
           .newTransformer()
           .transform(new DOMSource(element), new StreamResult(stringResult));
-      return stringResult.toString().replaceAll("<eligibility\\ xmlns=\"\">", "<eligibility>");
+      return stringResult.toString();
     } catch (SOAPException | TransformerException e) {
       throw new WriteFailed(e);
     }
