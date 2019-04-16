90e10fa77a537e428482160ca0612425e1ae19b8
jhulbert-va <43861345+jhulbert-va@users.noreply.github.com>
2019-04-16 13:48:59 -0400

    API-1125 unknown host exception (#6)

--- hand-of-queen-elizabeth/src/test/java/gov/va/api/health/queenelizabeth/ee/impl/ConnectionProviderTest.java
+++ hand-of-queen-elizabeth/src/test/java/gov/va/api/health/queenelizabeth/ee/impl/ConnectionProviderTest.java
@@ -27,10 +27,18 @@ public class ConnectionProviderTest {
   @Test(expected = Eligibilities.RequestFailed.class)
   @SneakyThrows
   public void localhostGetsRequestFailed() {
-    // URL url = Mockito.spy(new URL("http://ee.va.gov:9334/getEESummary/"));
     ConnectionProvider connectionProvider =
         new ConnectionProvider(
             new URL("https://localhost:9334/getEESummary/"), "test-truststore.jks", "secret");
+    connectionProvider.getConnection();
+  }
+
+  @Test(expected = Eligibilities.RequestFailed.class)
+  @SneakyThrows
+  public void unknownHostGetsRequestFailed() {
+    ConnectionProvider connectionProvider =
+        new ConnectionProvider(
+            new URL("https://ee.va.gov:9334/getEESummary/"), "test-truststore.jks", "secret");
     connectionProvider.getConnection();
   }
 }
