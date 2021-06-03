a22bae796c9a79626cf33c6a48c01a517db874f2
taylorrassmannlibertyits <64798603+taylorrassmannlibertyits@users.noreply.github.com>
2021-06-03 15:10:29 -0400

    Initial java 16 upgrade commit.

--- hand-of-queen-elizabeth/src/test/java/gov/va/api/health/queenelizabeth/ee/impl/EeSummaryEndpointConfigTest.java
+++ hand-of-queen-elizabeth/src/test/java/gov/va/api/health/queenelizabeth/ee/impl/EeSummaryEndpointConfigTest.java
@@ -1,20 +1,17 @@
 package gov.va.api.health.queenelizabeth.ee.impl;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Test loading an EeSummaryEndpointConfig configuration from properties and forcefully testing the
  * case where the url is not configured resulting in the default wsdl url usage.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(
     classes = EeSummaryEndpointConfigTest.TestConfiguration.class,
     initializers = ConfigFileApplicationContextInitializer.class)
